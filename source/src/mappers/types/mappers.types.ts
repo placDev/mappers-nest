3f2d3ea12fdbb090a2a24cc8435c66fae142e9a7
SuperPuperUser <aurstudio@gmail.com>
2025-02-22 20:57:39 +0300

    [~] fix

--- source/src/mappers/types/mappers.types.ts
+++ source/src/mappers/types/mappers.types.ts
@@ -1,3 +1,3 @@
 import { SettingsInterface } from '@mappers/core/dist/settings/interfaces/settings.interface';
 
-export type MapperSettingsNest = Omit<SettingsInterface, 'defaultValidator'>;
+export type MapperSettingsNest = Omit<SettingsInterface, 'collectType'>;
